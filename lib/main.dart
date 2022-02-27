50e94dae9ec58efbd5b2112ab56bfdee807deb1b
jh8057 <69341843+jh8057@users.noreply.github.com>
2022-02-27 12:28:06 +0900

    navigator & change emulator

--- lib/main.dart
+++ lib/main.dart
@@ -23,6 +23,7 @@ class MyHome extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       backgroundColor: Colors.amber[600],
       appBar: AppBar(
         title: const Text('ZZEMAL CARD'),
@@ -110,148 +111,162 @@ class MyHome extends StatelessWidget {
           ],
         ),
       ),
-      body: Padding(
-        padding: const EdgeInsets.fromLTRB(30, 40, 0, 0),
-        child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              const Center(
-                child: CircleAvatar(
-                  backgroundImage: AssetImage('assets/rainbowface.jpg'),
-                  radius: 60,
-                ),
-              ),
-              Divider(
-                height: 60,
-                color: Colors.grey[850],
-                thickness: 0.5,
-                endIndent: 30,
-              ),
-              const Text(
-                'Name',
-                style: TextStyle(
-                  color: Colors.white,
-                  letterSpacing: 2.0,
-                ),
-              ),
-              const SizedBox(
-                height: 10.0,
-              ),
-              const Text(
-                'ZZEMAL',
-                style: TextStyle(
-                  color: Colors.white,
-                  letterSpacing: 2.0,
-                  fontSize: 28.0,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              const SizedBox(
-                height: 30.0,
-              ),
-              const Text(
-                'CHARACTOR POWER LEVEL',
-                style: TextStyle(
-                  color: Colors.white,
-                  letterSpacing: 2.0,
-                ),
-              ),
-              const SizedBox(
-                height: 10.0,
-              ),
-              const Text(
-                '10.0',
-                style: TextStyle(
-                  color: Colors.white,
-                  letterSpacing: 2.0,
-                  fontSize: 28.0,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              TextButton(
-                onPressed: () {
-                  flutterToast();
-                },
-                child: const Text('Toast'),
-              ),
-              const SizedBox(
-                height: 30.0,
-              ),
-              Row(
-                children: const <Widget>[
-                  Icon(Icons.check_circle_outline),
-                  SizedBox(
-                    width: 10.0,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.fromLTRB(30, 40, 0, 0),
+          child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                const Center(
+                  child: CircleAvatar(
+                    backgroundImage: AssetImage('assets/rainbowface.jpg'),
+                    radius: 60,
                   ),
-                  Text(
-                    'memorize',
-                    style: TextStyle(
-                      letterSpacing: 1.0,
-                      fontSize: 16.0,
-                    ),
-                  )
-                ],
-              ),
-              Row(
-                children: const <Widget>[
-                  Icon(Icons.check_circle_outline),
-                  SizedBox(
-                    width: 10.0,
-                  ),
-                  Text(
-                    'using DART',
-                    style: TextStyle(
-                      letterSpacing: 1.0,
-                      fontSize: 16.0,
-                    ),
-                  )
-                ],
-              ),
-              Row(
-                children: const <Widget>[
-                  Icon(Icons.check_circle_outline),
-                  SizedBox(
-                    width: 10.0,
-                  ),
-                  Text(
-                    'make Flutter',
-                    style: TextStyle(
-                      letterSpacing: 1.0,
-                      fontSize: 16.0,
-                    ),
-                  )
-                ],
-              ),
-              Center(
-                child: CircleAvatar(
-                  backgroundImage: const AssetImage('assets/heart.png'),
-                  radius: 40,
-                  backgroundColor: Colors.amber[600],
                 ),
-              ),
-              Center(
-                child: TextButton(
-                  child: const Text(
-                    'Hello world',
-                    style: TextStyle(color: Colors.white),
+                Divider(
+                  height: 60,
+                  color: Colors.grey[850],
+                  thickness: 0.5,
+                  endIndent: 30,
+                ),
+                const Text(
+                  'Name',
+                  style: TextStyle(
+                    color: Colors.white,
+                    letterSpacing: 2.0,
                   ),
+                ),
+                const SizedBox(
+                  height: 10.0,
+                ),
+                const Text(
+                  'ZZEMAL',
+                  style: TextStyle(
+                    color: Colors.white,
+                    letterSpacing: 2.0,
+                    fontSize: 28.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const SizedBox(
+                  height: 30.0,
+                ),
+                const Text(
+                  'CHARACTOR POWER LEVEL',
+                  style: TextStyle(
+                    color: Colors.white,
+                    letterSpacing: 2.0,
+                  ),
+                ),
+                const SizedBox(
+                  height: 10.0,
+                ),
+                const Text(
+                  '10.0',
+                  style: TextStyle(
+                    color: Colors.white,
+                    letterSpacing: 2.0,
+                    fontSize: 28.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                TextButton(
                   onPressed: () {
-                    ScaffoldMessenger.of(context).showSnackBar(
-                      const SnackBar(
-                        content: Text(
-                          'Hello world',
-                          textAlign: TextAlign.center,
-                          style: TextStyle(color: Colors.white),
-                        ),
-                        backgroundColor: Colors.teal,
-                        duration: Duration(milliseconds: 1000),
-                      ),
-                    );
-                    print('text pressed');
+                    flutterToast();
                   },
+                  child: const Text('Toast'),
                 ),
-              )
-            ]),
+                const SizedBox(
+                  height: 30.0,
+                ),
+                Row(
+                  children: const <Widget>[
+                    Icon(Icons.check_circle_outline),
+                    SizedBox(
+                      width: 10.0,
+                    ),
+                    Text(
+                      'memorize',
+                      style: TextStyle(
+                        letterSpacing: 1.0,
+                        fontSize: 16.0,
+                      ),
+                    )
+                  ],
+                ),
+                Row(
+                  children: const <Widget>[
+                    Icon(Icons.check_circle_outline),
+                    SizedBox(
+                      width: 10.0,
+                    ),
+                    Text(
+                      'using DART',
+                      style: TextStyle(
+                        letterSpacing: 1.0,
+                        fontSize: 16.0,
+                      ),
+                    )
+                  ],
+                ),
+                Row(
+                  children: const <Widget>[
+                    Icon(Icons.check_circle_outline),
+                    SizedBox(
+                      width: 10.0,
+                    ),
+                    Text(
+                      'make Flutter',
+                      style: TextStyle(
+                        letterSpacing: 1.0,
+                        fontSize: 16.0,
+                      ),
+                    )
+                  ],
+                ),
+                Center(
+                  child: CircleAvatar(
+                    backgroundImage: const AssetImage('assets/heart.png'),
+                    radius: 40,
+                    backgroundColor: Colors.amber[600],
+                  ),
+                ),
+                Center(
+                  child: TextButton(
+                    child: const Text(
+                      'Hello world',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    onPressed: () {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        const SnackBar(
+                          content: Text(
+                            'Hello world',
+                            textAlign: TextAlign.center,
+                            style: TextStyle(color: Colors.white),
+                          ),
+                          backgroundColor: Colors.teal,
+                          duration: Duration(milliseconds: 1000),
+                        ),
+                      );
+                      print('text pressed');
+                    },
+                  ),
+                ),
+                Center(
+                  child: ElevatedButton(
+                    child: const Text('Navigator - GO NEXT PAGE'),
+                    onPressed: () {
+                      print('Click navigator');
+                      Navigator.push(context,
+                          MaterialPageRoute(builder: (BuildContext context) {
+                        return SecondPage();
+                      }));
+                    },
+                  ),
+                ),
+              ]),
+        ),
       ),
     );
   }
@@ -259,7 +274,7 @@ class MyHome extends StatelessWidget {
 
 void flutterToast() {
   Fluttertoast.showToast(
-    msg: 'Flutter',
+    msg: 'Flutter Toast',
     toastLength: Toast.LENGTH_SHORT,
     gravity: ToastGravity.BOTTOM,
     timeInSecForIosWeb: 1,
@@ -267,4 +282,28 @@ void flutterToast() {
     fontSize: 20.0,
     textColor: Colors.black,
   );
+}
+
+class SecondPage extends StatelessWidget {
+  const SecondPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext ctx) {
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      backgroundColor: Colors.amber[600],
+      appBar: AppBar(
+        title: const Text('Second Page'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          child: const Text('Navigator - GO NEXT PAGE'),
+          onPressed: () {
+            print('Click navigator');
+            Navigator.pop(ctx);
+          },
+        ),
+      ),
+    );
+  }
 }
