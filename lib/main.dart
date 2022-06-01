1dd278d2cbe875b6383ef5c1463f824528ffba80
jh8057 <luckysymbol13@gmail.com>
2022-06-02 01:36:39 +0900

    backup & appbar

--- lib/main.dart
+++ lib/main.dart
@@ -22,49 +22,68 @@ class MyHome extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('New Start'),
-        backgroundColor: Colors.brown,
-        elevation: 0.0, // appbar가 떠있는 정도
-      ),
-      body: Padding(
-        padding: const EdgeInsets.fromLTRB(30.0, 40.0, 0, 0),
-        child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Row(
-                children: [
-                  Column(children: const <Widget>[
-                    CircleAvatar(
-                      backgroundImage: AssetImage('assets/rainbow.png'),
+          title: const Text('New Start'),
+          backgroundColor: Colors.brown,
+          elevation: 0.0, // appbar가 떠있는 정도
+          leading:
+              // leading = left button
+              IconButton(
+            icon: const Icon(Icons.menu),
+            onPressed: () {
+              print('menu button is clicked');
+            },
+          ),
+          actions:
+              //  actions = right button
+              [
+            IconButton(
+              icon: const Icon(Icons.search),
+              onPressed: () {
+                print('menu search is clicked');
+              },
+            ),
+          ]),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.fromLTRB(30.0, 40.0, 0, 0),
+          child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Row(
+                  children: [
+                    Column(children: const <Widget>[
+                      CircleAvatar(
+                        backgroundImage: AssetImage('assets/rainbow.png'),
+                      ),
+                      Text(
+                        'name',
+                        style: TextStyle(
+                            color: Colors.brown,
+                            letterSpacing: 2.0,
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ]),
+                    const SizedBox(
+                      width: 10,
                     ),
-                    Text(
-                      'name',
-                      style: TextStyle(
-                          color: Colors.brown,
-                          letterSpacing: 2.0,
-                          fontWeight: FontWeight.bold),
+                    const Text(
+                      'ZZemal',
+                      style: TextStyle(color: Colors.black87, fontSize: 50),
                     ),
-                  ]),
-                  const SizedBox(
-                    width: 10,
-                  ),
-                  const Text(
-                    'ZZemal',
-                    style: TextStyle(color: Colors.black87, fontSize: 50),
-                  ),
-                ],
-              ),
-              const SizedBox(
-                height: 10,
-                width: 200,
-              ),
-              Row(
-                children: const <Widget>[
-                  Text('Why so serious?'),
-                  Icon(Icons.search),
-                ],
-              ),
-            ]),
+                  ],
+                ),
+                const SizedBox(
+                  height: 10,
+                  width: 200,
+                ),
+                Row(
+                  children: const <Widget>[
+                    Text('Why so serious?'),
+                    Icon(Icons.search),
+                  ],
+                ),
+              ]),
+        ),
       ),
     );
   }
