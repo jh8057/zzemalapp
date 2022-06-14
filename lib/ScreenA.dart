92b4a431a4aae8ecf4bd3719027e1430ee64a145
jh8057 <luckysymbol13@gmail.com>
2022-06-15 01:04:09 +0900

    setstate

--- lib/ScreenA.dart
+++ lib/ScreenA.dart
@@ -1,26 +1,40 @@
 import 'package:flutter/material.dart';
 
-class ScreenA extends StatelessWidget {
+class ScreenA extends StatefulWidget {
   const ScreenA({Key? key}) : super(key: key);
 
+  @override
+  State<ScreenA> createState() => _ScreenAState();
+}
+
+class _ScreenAState extends State<ScreenA> {
+  int counter = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
           title: const Text('ScreenA'),
         ),
-        body: Center(
-            child: ElevatedButton(
-          child: const Text('Go Back'),
-          onPressed: () {
-            Navigator.pop(context);
-          },
-        )),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Center(
+                child: ElevatedButton(
+              child: const Text('Go Back'),
+              onPressed: () {
+                Navigator.pop(context);
+              },
+            )),
+            Text('$counter'),
+          ],
+        ),
         floatingActionButton: FloatingActionButton(
-          child: const Icon(Icons.thumb_up),
+          child: const Icon(Icons.add),
           onPressed: () {
-            ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
-                content: Text('I LIKE IT!'), duration: Duration(seconds: 5)));
+            //flutter는 항상 rebuild해야 화면에 나오므로 이를 위한 setState()
+            setState(() {
+              counter++;
+            });
           },
         ));
   }
