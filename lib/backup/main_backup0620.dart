e505de828fd560c6f29eea94d53c68dfb70fd3a3
jh8057 <luckysymbol13@gmail.com>
2022-06-20 22:42:55 +0900

    backup

---
+++ lib/backup/main_backup0620.dart
@@ -0,0 +1,177 @@
+import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'zzemalApp',
+      theme: ThemeData(primarySwatch: Colors.grey),
+      home: const LogIn(),
+    );
+  }
+}
+
+class LogIn extends StatelessWidget {
+  const LogIn({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blue,
+        title: const Text('Sign In', style: TextStyle(color: Colors.white)),
+        centerTitle: true,
+        elevation: 0.2,
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              ButtonTheme(
+                  height: 50,
+                  child: TextButton(
+                    child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: const <Widget>[
+                          Icon(Icons.login_outlined),
+                          Text('Login with Google'),
+                        ]),
+                    onPressed: () {
+                      print('Google Login');
+                    },
+                  )),
+              ButtonTheme(
+                  height: 50,
+                  child: TextButton(
+                    child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: const <Widget>[
+                          Icon(Icons.login_outlined),
+                          Text('Naver'),
+                        ]),
+                    onPressed: () {
+                      print('Google Login');
+                    },
+                  ))
+            ]),
+      ),
+    );
+  }
+}
+
+///////////funtions////////////////////
+class MySnackBar extends StatelessWidget {
+  const MySnackBar({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: TextButton(
+        child: const Text('Show me'),
+        onPressed: () {
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(
+              content: const Text('Hello'),
+              backgroundColor: Color.fromARGB(255, 31, 58, 11),
+              action: SnackBarAction(
+                  label: 'More',
+                  onPressed: () {
+                    flutterToast('Clicked More!');
+                  }),
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10),
+                  side: const BorderSide(color: Colors.teal, width: 5)),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
+
+class MyToastBtn extends StatelessWidget {
+  const MyToastBtn({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: TextButton(
+        child: const Text('Toast'),
+        onPressed: () {
+          flutterToast('Toast Msg');
+        },
+      ),
+    );
+  }
+}
+
+class MyNavigator extends StatelessWidget {
+  const MyNavigator({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: TextButton(
+        child: const Text('Go Second Page'),
+        onPressed: () {
+          Navigator.push(context,
+              MaterialPageRoute(builder: (BuildContext context) {
+            return const MyContainer();
+          }));
+        },
+      ),
+    );
+  }
+}
+
+class MyContainer extends StatelessWidget {
+  const MyContainer({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey,
+      body: SafeArea(
+        child: Column(
+          children: [
+            Container(
+              color: Colors.blue,
+              width: 100,
+              height: 100,
+              margin: const EdgeInsets.all(20),
+              child: const Text('Hello Container'),
+            ),
+            TextButton(
+              child: const Text('Go Back Page'),
+              onPressed: () {
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+void flutterToast(msg) {
+  Fluttertoast.showToast(
+      msg: msg,
+      gravity: ToastGravity.BOTTOM,
+      backgroundColor: Colors.green,
+      fontSize: 20,
+      textColor: Colors.white,
+      toastLength: Toast.LENGTH_SHORT);
+}
+
+void judgeUser(controller, controller2) {
+  if (controller.text == 'dice' && controller2.text == '1234') {
+    print('You are my User');
+  }
+}
